1034aada12c36a25f1f3e4ccea26668b3fd31e92
H.Kubra.Cevirici <haticekubracevirici@gmail.com>
2026-09-04 13:46:04 +0200

    UserInMemoryRepository

--- Server/InMemoryRepository/UserInMemoryRepository.cs
+++ Server/InMemoryRepository/UserInMemoryRepository.cs
@@ -5,5 +5,50 @@ namespace InMemoryRepository;
 
 public class UserInMemoryRepository : IUserRepository
 {
-    
+    public Task<User> AddAsync(User user)
+    {
+        user.Id = users.Any()
+            ? users.Max(p => p.Id) + 1
+            : 1;
+        users.add(user);
+        return Task.FromResult(user);
+    }
+
+    public Task UpdateAsync(User user)
+    {
+        User? existingUser = users.SingleOrDefault(p => p.Id == user.Id);
+        if (existingUser is null)
+        {
+            throw new InvalidOperationException(
+                $"Post with ID '{user.Id}' not found")
+        }
+
+        users.Remove(existingUser);
+        users.Add(user);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(int id)
+    {
+        User? userToRemove = users.SingleOrDefault(p => p.Id == id);
+        if (userToRemove is null)
+        {
+            throw new InvalidOperationException(
+                $"User with ID '{id}' not found");
+        }
+
+        users.Remove(userToRemove);
+        return Task.CompletedTask;
+    }
+
+    public Task<User> GetSingleAsync(int id)
+    {
+        // Do implementation
+        return Task.FromResult(user);
+    }
+
+    public IQueryable<User> GetManyAsync()
+    {
+        return users.AsQueryable();
+    }
 }
